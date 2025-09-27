ca3095a726b1a284504878b6bacb967ead88cabd
Rekaprmna <rekka31@gmail.com>
2025-09-27 21:01:55 +0700

    update web.php lokal

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,22 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\product;
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-// Dashboard
-Route::get('/', function () {
-    return view('pages.dashboard');
-})->name('dashboard');
+Route::redirect('/', '/dashboard');
+
+//Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+//Route::post('/login', [LoginController::class, 'login'])->name('login.submit');
+//Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+
+// Dashboard hanya untuk user yang login
+Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', function () {
+        return view('pages.dashboard');
+    })->name('dashboard');
+});
 
 // Form Request User
 Route::get('/form-request-user', function () {
@@ -34,5 +43,7 @@ Route::get('/contact', function () {
     return view('pages.contact');
 })->name('contact');
 
-Route::get('/product',[ProductController::class,'index']);
-Route:get('/register')
+// Add.product
+Route::get('/add-product', function () {
+    return view('pages.add-product');
+})->name('add-product');
